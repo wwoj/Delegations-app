de00db5de67bfc5b0fb31f108d6fb238ad6c852a
wwoj <wojcik.wojtek@yahoo.pl>
2020-04-28 09:57:31 +0200

    Updates

---
+++ precache-manifest.4398605d5c4074ac8bf769c797a824f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7986795d141c5022a47e6a7ea6ce135b",
+    "url": "/Delegations-app/index.html"
+  },
+  {
+    "revision": "d7d2a88bd327e60c7069",
+    "url": "/Delegations-app/static/css/main.64ec075e.chunk.css"
+  },
+  {
+    "revision": "9e84203ba307e426f939",
+    "url": "/Delegations-app/static/js/2.6d949eff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Delegations-app/static/js/2.6d949eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d2a88bd327e60c7069",
+    "url": "/Delegations-app/static/js/main.830a571c.chunk.js"
+  },
+  {
+    "revision": "a5b1ea85ead1cf417843",
+    "url": "/Delegations-app/static/js/runtime-main.c91a055c.js"
+  },
+  {
+    "revision": "1361eaff8f9a1a8addf808032c6c2faa",
+    "url": "/Delegations-app/static/media/Logo_1.1361eaff.png"
+  },
+  {
+    "revision": "0ddce7924f1ae818a5f33246e574afaf",
+    "url": "/Delegations-app/static/media/employee.0ddce792.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Delegations-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
